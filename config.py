9704c73b8293a45f7b2ca7fe67ca394cbbaa5149
Tammy Wang <37206971+whale2018@users.noreply.github.com>
2018-08-01 01:38:09 -0700

    HW7

--- config.py
+++
@@ -1,5 +0,0 @@
-# Twitter API Keys
-consumer_key = "Vvof92CXdKIbXuzgetePhud5z"
-consumer_secret = "F9v8YkR3XX8RCQrPg2TjxnlX9wfSrna4TJwFwOCd544VxJgwSQ"
-access_token = "1019781747678986241-BSSG94WrgYNXrm7siYVwiBZHRkr6nV"
-access_token_secret = "kDKQcoBLGy1PTLSjsO6Ly0uqL1Rz2idwqbfbN5Pctxypn"
